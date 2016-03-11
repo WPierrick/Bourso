4c0529d839b5c9f3dccba81a97f241750d330bfa
DNAPierrick <w.pierrick@gmail.com>
2016-03-11 17:46:19 +0100

    Tkinter

--- bourse.py
+++ bourse.py
@@ -1,5 +1,48 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+'''
+"""Premier exemple avec Tkinter.
+
+On crée une fenêtre simple qui souhaite la bienvenue à l'utilisateur.
+
+"""
+
+# On importe Tkinter
+from tkinter import *
+
+# On crée une fenêtre, racine de notre interface
+fenetre = Tk()
+
+# On crée un label (ligne de texte) souhaitant la bienvenue
+# Note : le premier paramètre passé au constructeur de Label est notre
+# interface racine
+champ_label = Label(fenetre, text="Salut les Zér0s !")
+
+# On affiche le label dans la fenêtre
+champ_label.pack()
+
+# On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
+fenetre.mainloop()
+>>> champ_label["text"]
+'Salut les Zér0s !'
+>>> champ_label["text"] = "Maintenant, au revoir !"
+>>> champ_label["text"]
+'Maintenant, au revoir !'
+>>>
+'''
+'''
+
+from tkinter import *
+
+
+fenetre = Tk()
+
+label = Label(fenetre, text ="Hello World")
+
+label.pack()
+
+fenetre.mainloop()
+'''
 
 
 class action:
@@ -10,16 +53,16 @@ class action:
 
 	def dividendes(self):
 		newtot = self.rendement * self.nombreactions + self.nombreactions
-		print (newtot)
 		return newtot
 
 
 	def argenttotal(self):
 		toteur = self.nombreactions * self.cours
-		#print (toteur)
 		return toteur
 
-safran=action(10, 50, 0.013)
+
+
+safran=action(input("Nombre d'actions"), input("cours"), input("dividendes"))
 safran.dividendes()
 safran.argenttotal()
 
